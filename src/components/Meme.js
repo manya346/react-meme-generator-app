368e7cd4208e8545cf624860a81214d5e0fe3645
manya346 <khandelwalmanya346@gmail.com>
2024-12-12 01:32:20 +0530

    api-integration

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,5 +1,4 @@
 import React from "react";
-import memesData from "../memesData";
 
 export default function Meme() {
   const [meme, setMeme] = React.useState({
@@ -8,12 +7,19 @@ export default function Meme() {
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
 
-  const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+  const [allMemes, setAllMemes] = React.useState([])
+
+  React.useEffect(()=> {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(res=> res.json())
+      .then(data=> setAllMemes(data.data.memes))
+  }, [])
+
+  console.log(allMemes)
 
   function getMemeImage() {
-    const memesArray = allMemeImages.data.memes;
-    const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomNumber].url
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url
     setMeme(prevMeme => {
       return{
         ...prevMeme,
